3ae8ad74bdad131363971e5d651a3c8e0021807a
Johnny Lee-Othon <jleeothon@gmail.com>
2020-05-13 14:23:52 +0200

    🐠-fy bin

--- bin/reset-audio.sh
+++ bin/reset-audio.sh
@@ -1,3 +1,3 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 set -euxo pipefail
-sudo kill -9 `ps ax|grep 'coreaudio[a-z]' | awk '{print $1}'`
+sudo kill -9 "$(ps ax | grep 'coreaudio[a-z]' | awk '{print $1}')"

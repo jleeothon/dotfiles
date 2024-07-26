8943bf5355bf18a568ede8ee4320e8ef745a05a2
Johnny E. Lee-Othon <johnny.lee@wunderflats.com>
2024-07-26 12:13:47 +0200

    update bin

--- bin/flatten-folder.sh
+++ bin/flatten-folder.sh
@@ -5,4 +5,6 @@ set -eux
 LOGS_FILE="$HOME/.logs.flatten-folder.log"
 exec > "$LOGS_FILE" 2>&1
 
-find "$1" -type f -exec mv {} "$1" \;
+# find "$1" -type f -exec mv {} "$1" \;
+FOLDER_NAME="$1"
+find "$FOLDER_NAME" -mindepth 2 -type f -exec mv "$FOLDER_NAME" {} +

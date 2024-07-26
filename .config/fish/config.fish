59bc970452ea9b29e2a8253762503f4eca3b408c
Johnny E. Lee-Othon <johnny.lee@wunderflats.com>
2024-07-26 12:14:11 +0200

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,3 +38,5 @@ end
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
